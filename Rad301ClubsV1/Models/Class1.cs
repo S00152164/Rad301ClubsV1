c1a2488f31d6c5b3c2d90f3d92e10b90da11bef8
S00152164 <S00152164@VLAB-KCOMPY3-1>
2018-01-10 23:51:40 +0000

    test

---
+++ Rad301ClubsV1/Models/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Rad301ClubsV1.Models
+{
+    public class Class1
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Book ID")]
+        public int BookId { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "ISBN")]
+        public string ISBN { get; set; }
+
+        [Display(Name = "Author")]
+        public string Author { get; set; }
+
+       // public virtual ICollection<Loan> loanBooks { get; set; }
+    }
+}
---
+++ Rad301ClubsV1/Models/LibContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Rad301ClubsV1.Models
+{
+    public class LibContext : DbContext
+    {
+        public LibContext() : base("DefaultConnection")
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Class1> Books { get; set; }
+
+    }
+}
